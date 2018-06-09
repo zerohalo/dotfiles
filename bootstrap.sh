8c56754e1400a7d5b884b8357d6972acd48e80b6
Alan Graham <ag@zerohalo.com>
2018-06-09 16:00:37 -0400

    move dotconfig

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,4 +5,4 @@ rm .bashrc && ln -s "$HOME/dotfiles/home/bash/.bashrc" .bashrc
 rm .profile && ln -s "$HOME/dotfiles/home/bash/.profile" .profile
 rm .bash_logout && ln -s "$HOME/dotfiles/home/bash/.bash_logout" .bash_logout
 rm .tmux.conf && ln -s "$HOME/dotfiles/home/tmux/.tmux.conf" .tmux.conf
-rm -r .config/powerline && ln -s "$HOME/dotfiles/powerline" .config/powerline
+rm -r .config/powerline && ln -s "$HOME/dotfiles/dotconfig/powerline" .config/powerline
